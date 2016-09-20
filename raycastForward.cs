8c2b577dad0458f88b9034fb447f7f77815d2f47
AlfGuerrero <guerrero.alfrancis@gmail.com>
2016-09-20 12:34:33 -0400

    Create raycastForward.cs
    This file calculates distance between two objects.

---
+++ raycastForward.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class raycastForward : MonoBehaviour {
+	
+	void Update () {
+		RaycastHit hit;
+		float theDistance;
+		Vector3 forward = transform.TransformDirection(Vector3.forward) * 10; //10 meters
+		// Debug Raycast in the Editor - SO WE CAN SEE IT! 
+
+		Debug.DrawRay(transform.position, forward, Color.green);
+
+		if(Physics.Raycast(transform.position,(forward), out hit)){
+			theDistance = hit.distance; //Distance how far we hit
+
+			Debug.Log("DISTANCE HIT: "+theDistance + " " + "hit.collider.gameObject.name");
+		}
+	}
+}
