9ec863d480fe81393d7457d84d3731bcb2e9fa31
AlfGuerrero <guerrero.alfrancis@gmail.com>
2017-01-31 12:51:10 -0500

    Bluetooth New

--- Arduino-Alpha/Haptics_26/Haptics_26.ino
+++ Arduino-Alpha/Haptics_26/Haptics_26.ino
@@ -5,101 +5,84 @@
 #include "Arduino.h"
 #include "SoftwareSerial.h"
 
-
-// --------  connection
 SoftwareSerial mySerial(2, 3);
 String rawData;
 String rString;
 bool isReading = false;
 int arrayCount;
-float fbArray[8];
-float p_fbArray[8];
-byte index = 0; // Index into array; where to store the character
-
-// -------- Vib code
+//float fbArray[8];
+int p_fbArray[8];
+byte index = 0;
 float startTime;
 float i;
 int DUR = 4000;
 boolean mode = true;
 
-
-//MarkoSys * systemManager; //Top level manager of the vibration motors
-
 void setup() {
   Tlc.init();
-  //i = 4095;
   mySerial.begin(9600);
-  Serial.begin(9600);
-  Serial.write("Power On");
+  Serial.begin(19200);
   pinMode(13, OUTPUT);
-
   rawData = "";
   arrayCount = 0;
-
-  //  systemManager = new MarkoSys();
   for (int i = 0; i < 8; i++)
   {
-    fbArray[i] = 2;
+    //fbArray[i] = 4;
+    p_fbArray[i] = 4;
   }
+
+ 
+  mySerial.flush();
 }
 
 void loop()
 {
-  /*if (mySerial.available() > 0)
-    {
-    // Serial.println("Bluetooth Connected");
-    }
-    else {
-    //  Serial.println("Not Connected");
-    }*/
-
   if (mySerial.available() >= 8)
   {
-    //char c = (char)mySerial.read();  //gets one byte from serial buffer
-    //Serial.println(c);
-    //Serial.println(fbArray[0]);
     char c = mySerial.read();
-    if (c == 's')
+    if (c == 's' && isReading == false)
     {
-      //Serial.println("Packet received");
+      
       arrayCount = 0;
-      /*for (int i = 0; i < 8; i++)
-        {
-        fbArray[i] = 6;
-        }*/
+      isReading = true;
+      Serial.println(c);
     }
-    else if (c == ',') {
-
-      //Serial.println("frrabA "+ (String)arrayCount +": "+ fbArray[0]);
+    /*else if (c == ',') {
       fbArray[arrayCount] = rawData.toFloat();
       arrayCount++;
       rawData = "";
-
+      Serial.print(c);
+      }*/
+    else if (isReading == true) {
+      p_fbArray[arrayCount] = ((String)c).toInt() * 1000;
+      arrayCount++;
+      //Serial.print(c);
     }
-    else {
-      rawData += c;
-    }
+    mySerial.flush();
   }
 
-  if (arrayCount == 0) {
+  if (arrayCount >= 8 && isReading == true) {
+    //for (int i = 0; i < 8; i++) {
+    //if ((0.00 < fbArray[i]) && (fbArray[i] < 2.00))
+    //{
+    // p_fbArray[i] = fbArray[i];
+    Tlc.set(6, p_fbArray[0]);
+    Tlc.set(0, p_fbArray[3]);
+    Tlc.set(1, p_fbArray[4]);
+    Tlc.set(2, p_fbArray[2]);
+    Tlc.set(3, p_fbArray[5]);
+    Tlc.set(4, p_fbArray[7]);
+    Tlc.set(5, p_fbArray[1]);
+    Tlc.set(15, p_fbArray[7]);
+    Tlc.update();
 
-    for (int i = 1; i <= 8; i++) {
-
-      if ((0.00 < fbArray[i]) && (fbArray[i] < 4.00))
-      {
-        Tlc.set(6, map(fbArray[1], 0, 2, 0, 4095));
-        Tlc.set(0, map(fbArray[4], 0, 2, 0, 4095));
-        Tlc.set(1, map(fbArray[5], 0, 2, 0, 4095));
-        Tlc.set(2, map(fbArray[3], 0, 2, 0, 4095));
-        Tlc.set(3, map(fbArray[6], 0, 2, 0, 4095));
-        Tlc.set(4, map(fbArray[7], 0, 2, 0, 4095));
-        Tlc.set(5, map(fbArray[2], 0, 2, 0, 4095));
-        Tlc.set(15, map(fbArray[8], 0, 2, 0, 4095));
-        Tlc.update();
-        Serial.println("DATA " + (String)i + ": " + fbArray[i]);
-      }
+    isReading = false;
+    //}
+    //else
+    //p_fbArray[i] = 2.0;
+    //}
+    for (int i = 0; i < 8; i++) {
+      Serial.println("DATA " + (String)i + ": " + p_fbArray[i]);
     }
-
-
   }
 }
